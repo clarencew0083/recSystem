c679641c2236d009c72f430a799fd62b06fefc79
Clarence Williams <clarencew3@gmail.com>
2020-01-29 09:12:53 -0500

    adds new files

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' @title  699 Credit Card Fraud data set
+#' @name psc
+#' @description 
+#' A very large dataset
+#' 
+#' @format A \code{tibble} with x rows and x columns
+#' 
+#' \describe{
+#'    \item{col1}{something}
+#'    \item{col2}{something}
+#'    \item{col3}{something}
+#'    }
+#'
+NULL
